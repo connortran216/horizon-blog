25ee899c5c792be61587969e5801937b1f25c477
Connor Tran <connortran123@gmail.con>
2026-03-20 12:51:10 +0700

    refactor(animations): remove animation settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
-import { AccessibilityProvider } from './core'
 import Routes from './Routes'
 import AppLayout from './app/layouts/AppLayout'
 import theme from './theme'
@@ -11,15 +10,13 @@ function App() {
     <>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <ChakraProvider theme={theme}>
-        <AccessibilityProvider enablePanel={true}>
-          <AuthProvider>
-            <Router>
-              <AppLayout>
-                <Routes />
-              </AppLayout>
-            </Router>
-          </AuthProvider>
-        </AccessibilityProvider>
+        <AuthProvider>
+          <Router>
+            <AppLayout>
+              <Routes />
+            </AppLayout>
+          </Router>
+        </AuthProvider>
       </ChakraProvider>
     </>
   )
